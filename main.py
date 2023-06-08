ef67023b03a237dcf5b55a87b846120d88ac8e43
Jesse Visser <jssvssr2000@gmail.com>
2023-06-08 16:49:28 +0200

    Added option to run on a directory

--- main.py
+++ main.py
@@ -1,44 +1,46 @@
 import sys
 import argparse
+import os
 
 import torch
-import numpy as np
 from pathlib import Path
 from matplotlib import pyplot as plt
+import numpy as np
+from tqdm import tqdm
 
-from maskformer_segment import segment_with_maskformer
-from lama_inpaint import inpaint_with_lama
+from maskformer import MaskFormer
+from lama import LaMa
 from utils import load_img_to_array, save_array_to_img, dilate_mask, show_mask
 
 
 def setup_args(parser):
     parser.add_argument(
-        "--input_img", type=str, required=True,
-        help="Path to a single input img",
+        "input",  type=str, default=None,
+        help="Path to a single input image",
     )
     parser.add_argument(
-        "--labels", type=str, required=True, nargs='+',
+        "-l", "--labels", type=str, required=True, nargs='+',
         help="The labels of objects to remove",
+    )
+    parser.add_argument(
+        "-o", "--output_dir", type=str, default="./results",
+        help="Output path to the directory with results.",
     )
     parser.add_argument(
         "--dilate_kernel_size", type=int, default=15,
         help="Dilate kernel size.",
     )
     parser.add_argument(
-        "--output_dir", type=str, default="./results",
-        help="Output path to the directory with results.",
-    )
-    parser.add_argument(
         "--save_masks", type=bool, default=False,
         help="Whether to save the masks to image files."
     )
     parser.add_argument(
-        "--lama_config", type=str, default="./lama_config.yaml",
-        help="The path to the config file of lama model.",
-    )
-    parser.add_argument(
         "--lama_ckpt", type=str, default="./pretrained_models/big-lama",
         help="The path to the lama checkpoint.",
+    )
+    parser.add_argument(
+        "--lama_config", type=str, default="./lama_config.yaml",
+        help="The path to the config file of lama model.",
     )
     parser.add_argument(
         "--maskformer_ckpt", type=str, default="facebook/maskformer-swin-large-ade",
@@ -50,15 +52,49 @@ def setup_args(parser):
     )
 
 
-def save_masked_image(img, mask, img_mask_p):
+def save_masked_image(image: np.ndarray, mask: np.ndarray, img_mask_path: str | os.PathLike):
     dpi = plt.rcParams['figure.dpi']
-    height, width = img.shape[:2]
+    height, width = image.shape[:2]
     plt.figure(figsize=(width / dpi / 0.77, height / dpi / 0.77))
-    plt.imshow(img)
+    plt.imshow(image)
     show_mask(plt.gca(), mask, random_color=False)
     plt.axis('off')
-    plt.savefig(img_mask_p, bbox_inches='tight', pad_inches=0)
+    plt.savefig(img_mask_path, bbox_inches='tight', pad_inches=0)
     plt.close()
+
+
+def remove_objects_from_image(maskformer: MaskFormer, lama: LaMa, input_path: str | os.PathLike,
+                              output_dir: str | os.PathLike, dilate_kernel_size: int):
+    image = load_img_to_array(input_path)
+    masks, labels = maskformer.segment(image, args.labels)
+
+    out_dir = Path(output_dir)
+    img_stem = Path(input_path).stem
+    if args.save_masks: out_dir = out_dir / img_stem
+    out_dir.mkdir(parents=True, exist_ok=True)
+
+    # Dilate mask to avoid unmasked edge effect
+    if dilate_kernel_size is not None:
+        masks = [dilate_mask(mask, args.dilate_kernel_size) for mask in masks]
+
+    # Loop over masks and do in-painting for each selected label
+    for mask, label in zip(masks, labels):
+        if args.save_masks:
+            # Save the mask
+            mask_p = out_dir / f"mask_{label}.png"
+            save_array_to_img(mask, mask_p)
+
+            # Save the masked image
+            img_mask_p = out_dir / f"with_mask_{label}.png"
+            save_masked_image(image, mask, img_mask_p)
+
+        # Inpaint mask and save image
+        img_inpainted = lama.inpaint(image, mask)
+        image = img_inpainted
+
+    # Save final result
+    img_final_p = out_dir / f"{img_stem}.png"
+    save_array_to_img(image, img_final_p)
 
 
 if __name__ == "__main__":
@@ -67,33 +103,16 @@ if __name__ == "__main__":
     args = parser.parse_args(sys.argv[1:])
 
     device = "cuda" if torch.cuda.is_available() else "cpu"
-    img = load_img_to_array(args.input_img)
+    maskformer = MaskFormer(args.maskformer_ckpt, args.label_file)
+    lama = LaMa(args.lama_ckpt, args.lama_config)
 
-    masks, labels = segment_with_maskformer(img, args.maskformer_ckpt, args.labels, args.label_file)
-
-    # dilate mask to avoid unmasked edge effect
-    if args.dilate_kernel_size is not None:
-        masks = [dilate_mask(mask, args.dilate_kernel_size) for mask in masks]
-
-    img_stem = Path(args.input_img).stem
-    out_dir = Path(args.output_dir)
-    if args.save_masks: out_dir = out_dir / img_stem
-    out_dir.mkdir(parents=True, exist_ok=True)
-
-    # Loop over masks and do in-painting for each selected label
-    for mask, label in zip(masks, labels):
-        if args.save_masks:
-            # save the mask
-            mask_p = out_dir / f"mask_{label}.png"
-            save_array_to_img(mask, mask_p)
-
-            # save the masked image
-            img_mask_p = out_dir / f"with_mask_{label}.png"
-            save_masked_image(img, mask, img_mask_p)
-
-        # Inpaint mask and save image
-        img_inpainted = inpaint_with_lama(img, mask, args.lama_config, args.lama_ckpt, device=device)
-        img = img_inpainted
-
-    img_final_p = out_dir / f"{img_stem}.png"
-    save_array_to_img(img, img_final_p)
+    if not os.path.exists(args.input):
+        raise IOError(f"{args.input} does not exist.")
+    if os.path.isdir(args.input):
+        images = [entry.path for entry in os.scandir(args.input) if entry.is_file()]
+        if len(images) == 0:
+            raise IOError(f"There are no files in {args.input}.")
+        for image in tqdm(images):
+            remove_objects_from_image(maskformer, lama, image, args.output_dir, args.dilate_kernel_size)
+    else:
+        remove_objects_from_image(maskformer, lama, args.input, args.output_dir, args.dilate_kernel_size)
---
+++ maskformer.py
@@ -0,0 +1,33 @@
+import json
+import os
+
+from transformers import MaskFormerForInstanceSegmentation, MaskFormerImageProcessor
+from torch import index_select, tensor
+import numpy as np
+
+
+class MaskFormer:
+    def __init__(self, ckpt: str | os.PathLike, label_file: str | os.PathLike):
+        self.model = MaskFormerForInstanceSegmentation.from_pretrained(ckpt)
+        self.processor = MaskFormerImageProcessor()
+        self.labels = json.load(open(label_file, "r"))
+
+    def segment(self, image: np.ndarray, to_select: list[str]):
+        inputs = self.processor(images=image, return_tensors="pt")
+        outputs = self.model(**inputs)
+        batch_size = outputs.class_queries_logits.shape[0]
+        outputs = self.processor.post_process_instance_segmentation(
+            outputs,
+            target_sizes=[image.shape[:2] for _ in range(batch_size)],
+            return_binary_maps=True
+        )[0]
+
+        mask_selected = []
+        mask_labels = []
+        for item in outputs['segments_info']:
+            label = self.labels[str(item['label_id'])]
+            if label in to_select:
+                mask_selected.append(item['id'])
+                mask_labels.append(label)
+        masks = index_select(outputs['segmentation'], 0, tensor(mask_selected)).numpy().astype(np.uint8) * 255
+        return masks, mask_labels
