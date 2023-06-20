97cfdbc7453a613d6e81895b10585ea5fd7178fd
Jesse Visser <jssvssr2000@gmail.com>
2023-06-20 13:25:19 +0200

    Added packaging files

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="semremover",
+    version="{{VERSION}}",
+    packages=find_packages(),
+    package_data={'semremover': ["models/*/*.json, models/*/*.yaml"]},
+    install_required=[
+        "LaMa @ git+https://github.com/jessseee/lama.git",
+        "torch~=2.0",
+        "transformers~=4.30",
+        "omegaconf~=2.3",
+        "opencv-python",
+        "numpy",
+        "pillow",
+        "argparse",
+        "tqdm",
+    ]
+)
